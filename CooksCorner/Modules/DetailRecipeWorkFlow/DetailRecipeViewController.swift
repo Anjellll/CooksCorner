ddd9bec043d05f40d7fcd3a7bec19f564ca22eb7
Anjella <anjellakattoobekova@gmail.com>
2024-03-17 02:34:34 +0600

    Changed the navigationBar

--- CooksCorner/Modules/DetailRecipeWorkFlow/DetailRecipeViewController.swift
+++ CooksCorner/Modules/DetailRecipeWorkFlow/DetailRecipeViewController.swift
@@ -25,15 +25,25 @@ class DetailRecipeViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .red
         updateUI()
+        setupNavBar()
     }
-    
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension DetailRecipeViewController {
     private func updateUI() {
         if let recipe = viewModel.recipe {
             contentView.updateView(recipe: recipe)
         }
     }
 
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    private func setupNavBar() {
+        if let navigationController = self.navigationController {
+            navigationController.navigationBar.tintColor = .white
+            navigationController.navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        }
     }
 }
